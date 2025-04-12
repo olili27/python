05d50bb6f387fc8c2377e6656021af9bf9598e40
olili27 <timmolili27@gmail.com>
2025-04-12 15:16:14 +0300

    test with pytest.raises

--- practice/testing/unit-tests-with-pytest/test_user_manager.py
+++ practice/testing/unit-tests-with-pytest/test_user_manager.py
@@ -7,4 +7,8 @@ def user_manager():
     return UserManager()
 
 def test_add_user(user_manager):
-    assert user_manager.add_user("tim", "tim@gmail.com") == True
+    assert user_manager.add_user("tim", "tim@gmail.com") == True, 'add_user("tim", "tim@gmail.com")'
+
+def test_add_duplicate_user(user_manager):
+    with pytest.raises(ValueError, match="user already exists"):
+        user_manager.add_user("tim", "tim@gmail.com")
