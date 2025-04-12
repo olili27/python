615036476ae85373c6217b2639571fbc8aaba965
olili27 <timmolili27@gmail.com>
2025-04-12 15:22:37 +0300

    test out more things about pytest.fixture

--- practice/testing/unit-tests-with-pytest/test_user_manager.py
+++ practice/testing/unit-tests-with-pytest/test_user_manager.py
@@ -10,5 +10,9 @@ def test_add_user(user_manager):
     assert user_manager.add_user("tim", "tim@gmail.com") == True, 'add_user("tim", "tim@gmail.com")'
 
 def test_add_duplicate_user(user_manager):
+    # user_manager.add_user("timo", "tim@gmail.com")
+    # with pytest.raises(ValueError, match="user already exists"):
+    #     user_manager.add_user("time", "tim@gmail.com")
+
     with pytest.raises(ValueError, match="user already exists"):
         user_manager.add_user("tim", "tim@gmail.com")
